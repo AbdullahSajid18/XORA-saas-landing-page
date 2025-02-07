e608d6e3db90922c17561bffeb2d2622950450bb
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-02-07 17:56:33 +0500

    implement header and hero sections

---
+++ src/sections/Header.jsx
@@ -0,0 +1,95 @@
+import clsx from "clsx";
+import { useState } from "react";
+import { Link as LinkScroll } from "react-scroll";
+
+const Navlink = ({ title }) => (
+  <LinkScroll className="uppercase transition-colors duration-500 cursor-pointer base-bold text-p4 hover:text-p1 max-lg:my-4 max-lg:h5">
+    {title}
+  </LinkScroll>
+);
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex items-center h-14 max-lg:px-5">
+        <a className="flex-1 cursor-pointer lg:hidden z-2">
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+        </a>
+
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+          )}
+        >
+          <div className=" max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <Navlink title="features" />
+                  <div className="dot" />
+                  <Navlink title="pricing" />
+                </li>
+
+                <li className="nav-logo">
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                    )}
+                  >
+                    <img
+                      src="/images/xora.svg"
+                      width={160}
+                      height={55}
+                      alt="logo"
+                    />
+                  </LinkScroll>
+                </li>
+                <li className="nav-li">
+                  <Navlink title="faq" />
+                  <div className="dot" />
+                  <Navlink title="download" />
+                </li>
+              </ul>
+            </nav>
+
+            <div className="absolute left-0 block lg:hidden top-1/2 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="flex items-center justify-center border-2 rounded-full lg:hidden z-2 size-10 border-s4/25"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <img
+            src={`/images/${isOpen ? "close" : "magic"}.svg`}
+            className="object-contain size-1/2"
+          />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
