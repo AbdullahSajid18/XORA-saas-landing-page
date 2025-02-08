01f1b361e9135eadff24496fd5e7e30bda481186
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-02-08 23:03:58 +0500

    Implement the faq and testimonial section

---
+++ src/sections/Download.jsx
@@ -0,0 +1,19 @@
+import { Element } from "react-scroll"
+
+const Download = () => {
+  return (
+    <section>
+        <Element className="relative pt-24 pb-32 g7 max-lg:pb-24 max-md:py-16">
+            <div className="container">
+                <div className="flex items-center">
+                    <div className="relative mr-6 flex-540 max-xl:flex-280 max-lg:flex256 max-md:flex-100">
+
+                    </div>
+                </div>
+            </div>
+        </Element>
+    </section>
+  )
+}
+
+export default Download
